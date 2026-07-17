1c4ad1642a5e0eba9eb3140ff5be9d82bb75e8f4
nat <nitishasharma1401@gmail.com>
2026-07-17 16:45:02 +0530

    added filtering to mats

--- genshin-react-project/src/pages/materials.tsx
+++ genshin-react-project/src/pages/materials.tsx
@@ -1,9 +1,44 @@
 import { materials } from "../data/materials";
+import { useState } from "react";
+import FilterGroup from "../components/filtering";
 
 export default function MaterialList() {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [selectedRegions, setSelectedRegions] = useState<string[]>([]);
+
+  const [selectedCats, setSelectedCats] = useState<string[]>([]);
+
+  const filteredMats = materials.filter((mat) => {
+    const regionMatch =
+      selectedRegions.length === 0 ||
+      mat.regions.some((region) => selectedRegions.includes(region));
+
+    const catMatch =
+      selectedCats.length === 0 || selectedCats.includes(mat.category);
+    return regionMatch && catMatch;
+  });
   return (
     <div>
-      {materials.map((mat) => (
+      <button onClick={() => setShowFilters(!showFilters)}>Filters here</button>
+      {showFilters && (
+        <div className="filter-panel">
+          <FilterGroup
+            title="Regions"
+            selected={selectedRegions}
+            options={["Mondstadt", "Liyue", "Inazuma", "Sumeru", "Fontaine"]}
+            onChange={setSelectedRegions}
+          />
+
+          <FilterGroup
+            title="Category"
+            selected={selectedCats}
+            options={["Common", "Local specialty", "Boss drop"]}
+            onChange={setSelectedCats}
+          />
+        </div>
+      )}
+      {filteredMats.map((mat) => (
         <div key={mat.id}>
           <h1>{mat.name}</h1>
           <p>
